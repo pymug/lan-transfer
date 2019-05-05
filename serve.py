15d73073c645ca7799bc0ab814faf65b751f8bf3
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2019-05-05 11:15:30 +0000

    cross-platforming the script

--- serve.py
+++ serve.py
@@ -12,5 +12,5 @@ print('serving on:', IP)
 print ('IP Address of server: ', socket.gethostbyaddr(IP))
 
 Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(('', 5000), Handler) as httpd:
-    httpd.serve_forever()
+httpd =  socketserver.TCPServer(('', 5000), Handler)
+httpd.serve_forever()
